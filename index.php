fd56192d46cc4b917369ba2db3bf7b3e70cd5496
Mithila <mithilamadusanka97@gmail.com>
2020-12-05 14:18:38 +0530

    finish

--- index.php
+++ index.php
@@ -9,43 +9,40 @@
 		<aside id="fh5co-hero">
 			<div class="flexslider">
 				<ul class="slides">
-		   			<li style="background-image: url(images/faculty.jpg);">
+		   			<li style="background-image: url(images/fac3.jpeg);">
 		   				<div class="overlay-gradient"></div>
 		   				<div class="container">
 		   					<div class="row">
 			   					<div class="col-md-8 col-md-offset-2 text-center slider-text">
 			   						<div class="slider-text-inner">
-			   							<h1>The Roots of Education are Bitter, But the Fruit is Sweet</h1>
-										<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-										<p><a class="btn btn-primary btn-lg" href="#">Start Learning Now!</a></p>
+			   							<h1>To be excellent in nurturing the nations experts for the next generation of technology</h1>
+										
 			   						</div>
 			   					</div>
 			   				</div>
 		   				</div>
 		   			</li>
-		   			<li style="background-image: url(images/img_bg_2.jpg);">
+		   			<li style="background-image: url(images/fac2.jpeg);">
 		   				<div class="overlay-gradient"></div>
 		   				<div class="container">
 		   					<div class="row">
 			   					<div class="col-md-8 col-md-offset-2 text-center slider-text">
 			   						<div class="slider-text-inner">
-			   							<h1>The Great Aim of Education is not Knowledge, But Action</h1>
-										<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-										<p><a class="btn btn-primary btn-lg btn-learn" href="#">Start Learning Now!</a></p>
+			   							<h1>To produce accredited professional technologists to meet the needs of the world of technology</h1>
+										
 			   						</div>
 			   					</div>
 			   				</div>
 		   				</div>
 		   			</li>
-		   			<li style="background-image: url(images/img_bg_3.jpg);">
+		   			<li style="background-image: url(images/fac4.jpg);">
 		   				<div class="overlay-gradient"></div>
 		   				<div class="container">
 		   					<div class="row">
 			   					<div class="col-md-8 col-md-offset-2 text-center slider-text">
 			   						<div class="slider-text-inner">
 			   							<h1>We Help You to Learn New Things</h1>
-										<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
-										<p><a class="btn btn-primary btn-lg btn-learn" href="#">Start Learning Now!</a></p>
+										
 			   						</div>
 			   					</div>
 			   				</div>
