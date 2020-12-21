a0fd1168b695ec766353485087e328c8673403c8
Mithila <mithilamadusanka97@gmail.com>
2020-12-21 21:24:41 +0530

    updatd

--- header_navbar.php
+++ header_navbar.php
@@ -6,14 +6,14 @@
 					<div class="col-xs-12 text-right">
 						<p class="site"><font color="white">www.tec.ruh.ac.lk</font></p>
 							<ul class="fh5co-social">
-								<li><a href="#">Zoom Meetings(LEARN)</a></li>
+								<li><a href="page404.php">Zoom Meetings(LEARN)</a></li>
 								<li><a href="lms.php">LMS</a></li>
 								<li><a href="tecmis.php">TECMIS</i></a></li>
 								<li><a href="https://www.lib.ruh.ac.lk/web/index.php" target="_blank">Library</i></a></li>
 							</ul>
 							<ul class="fh5co-social">
-								<li><a href="#"><i class="icon-facebook2"></i></a></li>
-								<li><a href="#"><i class="icon-twitter2"></i></a></li>
+								<li><a href="page404.php"><i class="icon-facebook2"></i></a></li>
+								<li><a href="page404.php"><i class="icon-twitter2"></i></a></li>
 							</ul>
 					</div>
 				</div>
@@ -52,12 +52,12 @@
 							<li class="has-dropdown">
 								<a href="">Campus Life</a>
 								<ul class="dropdown" style="margin-top:-7px;">
-									<li><a href="#">Sports</a></li>
-									<li><a href="#">Career Guidance</a></li>
-									<li><a href="#">Counseling</a></li>
-									<li><a href="#">Cultural Centre</a></li>
-									<li><a href="#">Medical Facilities</a></li>
-									<li><a href="#">Accommodation</a></li>
+									<li><a href="page404.php">Sports</a></li>
+									<li><a href="page404.php">Career Guidance</a></li>
+									<li><a href="page404.php">Counseling</a></li>
+									<li><a href="page404.php">Cultural Centre</a></li>
+									<li><a href="page404.php">Medical Facilities</a></li>
+									<li><a href="page404.php">Accommodation</a></li>
 								</ul>
 							</li>
 							<li class="has-dropdown">
@@ -65,19 +65,19 @@
 								<ul class="dropdown" style="margin-top:-7px;">
 									<li><a href="academic_calender.php">Academic Calender</a></li>
 									<li><a href="resources/handbook.pdf" target="_blank">Students Handbook</a></li>
-									<li><a href="#">Time Table</a></li>
+									<li><a href="page404.php">Time Table</a></li>
 									<li><a href="https://www.lib.ruh.ac.lk/web/index.php" target="_blank">University Library</a></li>
-									<li><a href="#">Medical Facilities</a></li>
+									<li><a href="page404.php">Medical Facilities</a></li>
 									<li><a href="lms.php">LMS</a></li>
 								</ul>
 							</li>
 							<li class="has-dropdown">
 								<a href="">Staff</a>
 								<ul class="dropdown" style="margin-top:-7px;">
-									<li><a href="#" class="hov">Staff Details</a></li>
-									<li><a href="#">Foreign Leave Request System</a></li>
-									<li><a href="#">Foreign Leave Approval System</a></li>
-									<li><a href="#">Vacancies</a></li>
+									<li><a href="page404.php" class="hov">Staff Details</a></li>
+									<li><a href="page404.php">Foreign Leave Request System</a></li>
+									<li><a href="page404.php">Foreign Leave Approval System</a></li>
+									<li><a href="page404.php">Vacancies</a></li>
 									
 								</ul>
 							</li>
